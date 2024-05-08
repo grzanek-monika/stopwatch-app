dbd08e3ba37fa55586263357c3b2772b66eb1bdc
Monika Grzanek <grzanekmonika@o2.pl>
2024-05-08 14:10:40 +0200

    Add Button component, add useState and useEffect in App, add msToTime in Timer component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
+import Timer from "./Timer/Timer.js";
+import Button from "./Button/Button.js";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [time, setTime] = useState(0);
+  const [timer, setTimer] = useState(false);
+
+
+  useEffect(() => {
+    let interval = null;
+
+    if(timer) {
+      interval = setInterval(() => {
+        setTime((prevValue) => prevValue + 10);
+      }, 10)
+    } else if(!timer) {
+      clearInterval(interval);
+    }
+  })
+ 
   return (
     <div>
-      
+        <Timer time={time} />
+        <Button onClick={setTimer(() => setTimer(true))}>Start</Button>
+        <Button onClick={setTimer(() => setTimer(false))}>Stop</Button>
+        <Button onClick={setTimer(() => setTimer(0))}>Reset</Button>
     </div>
   );
 }
---
+++ src/Timer/Timer.js
@@ -0,0 +1,25 @@
+import styles from './Timer.module.scss';
+
+const Timer = props => {
+
+    const msToTime = duration => {
+        let milliseconds = parseInt((duration%1000)/100)
+        , seconds = parseInt((duration/1000)%60)
+        , minutes = parseInt((duration/(1000*60))%60)
+        , hours = parseInt((duration/(1000*60*60))%24);
+
+        hours = (hours < 10) ? "0" + hours : hours;
+        minutes = (minutes < 10) ? "0" + minutes : minutes;
+        seconds = (seconds < 10) ? "0" + seconds : seconds;
+
+        return hours + ":" + minutes + ":" + seconds + "." + milliseconds;
+    }
+
+    return(
+        <div>
+            {msToTime(props.time)}
+        </div>
+    )
+}
+
+export default Timer;
